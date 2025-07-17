359a43aeaa286b3edfff0610a6adff1194f8065e
Johnny Talbot <johnnywtalbot@gmail.com>
2025-07-17 17:35:51 +0800

    fix button centering mobile

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Button({ children, className = '', ...props }: React.ButtonHTMLAttributes<HTMLButtonElement> & React.HTMLAttributes<HTMLDivElement>) {
   return(
     <button 
-      className={`custom-button flex flex-col text-light px-3 lg:px-5 py-2 cursor-pointer ${className}`}
+      className={`custom-button flex flex-col items-center justify-center text-center text-light px-3 lg:px-5 py-2 cursor-pointer ${className}`}
       {...props}
     >
       {children}
